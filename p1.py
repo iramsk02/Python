f7e8a4769a9a6a1d580e0fd85cc795a9a9905479
iramsk02 <161953870+iramsk02@users.noreply.github.com>
2024-10-13 20:49:42 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,141 @@
+#this is a single line comment
+'''
+this
+is 
+a multi
+line 
+comment
+'''
+"""
+this
+is 
+a multi
+line 
+comment
+"""
+# print("hello")
+# print("hello")
+
+# a=4
+# a="hi"
+# x=9.8
+# y=True
+# A=5
+# _a=4
+
+# print(type(a))
+# print(type(x))
+# print(type(y))
+
+# studentName="Dev"#camel case
+# StudentName="Dev"#Pascal case
+# Student_Name="Dev"#Snake case
+# for=3 error
+# 1a=4 error
+
+# print(a)
+# print(A)
+
+# x,y,z="orange",1,True #assigning values to multiple variable in single line
+# print(x,y,z)
+
+# x=y=z=0 #assigning same value to multiple variable
+# print(x,y,z)
+
+# Fruits=["mango","apple","banana"]
+# x,y,z=Fruits
+# print(x,y,z)
+
+# x=4#global variable
+
+# def myfun():
+#     # x=5 #local
+#     global x
+#     x=5
+#     print(x)
+
+# myfun()
+# print(x)
+
+# x=1+5j
+# print(x)
+
+# x=int(input("enter value of x "))
+# y=int(input("enter value of y "))
+
+
+# //print(x+y)
+
+# import random
+# print(random.randrange(1,10))   
+
+# x=int(1)
+# print(x)
+# x=int(1.2)
+# print(x)
+# # x=int("hi")  #error
+# # print(x)
+# x=int("3")
+# print(x)
+
+
+
+# x=float(1)
+# print(x)
+# x=float(1.2)
+# print(x)
+# # x=float("hi") #error
+# # print(x)
+# x=float("33")
+# print(x)
+
+
+# x=str(1)
+# print(x)
+# x=str(1.2)
+# print(x)
+# x=str("hi")
+# print(x)
+# x=str("3")
+# print(x)
+
+
+#STRING
+
+# a="'hi'"
+# print(a)
+
+# a='"hello"'
+# print(a)
+
+
+# a="hello azhar"
+# print(a[0])
+# print(a[3])
+
+# l=len(a)
+# print(l)
+# print(len(a))
+
+# for x in "Banana":
+#     print(x)
+
+# txt="Mohd Azharuddin Multani is a cute boy"
+# # print("cute" in txt) # true or false
+
+# if "girl" in txt:
+#     print("yes")
+# else:
+#     print("no")
+
+# slicing in string
+
+    
+a="hello azhar"
+# print(a[3:8]) # slice in range
+# print(a[:8])   # slice from 0th index to range
+# print(a[3:])  # slice from range to last index
+
+#negative indexing
+print(a[-5:-1])
+
